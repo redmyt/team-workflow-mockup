90b62d818e1fd40d98c5759b906b3d7f399fc7a2
Ira Bautista <Ira Bautista>
2017-05-09 17:07:05 +0300

    'add-includePath-for-normalize-sass-task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var autopref = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     gulp.src('./src/sass/main.scss')
-        .pipe( sass() )
+        .pipe( sass({
+      // includePaths: require('node-normalize-scss').with('other/path', 'another/path') 
+      // - or - 
+      includePaths: require('node-normalize-scss').includePaths
+    } )
         .pipe( autopref() )
         .pipe( gulp.dest('build/css') );
 });
