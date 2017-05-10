fa24afdb52bd38e0ef3012c51a59a8344ccfbb51
dmytrorevak <dmytro27@gmail.com>
2017-05-10 15:00:08 +0300

    add img task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     pug = require('gulp-pug'),
-    autopref = require('gulp-autoprefixer');
+    autopref = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function() {
     gulp.src('./src/sass/main.scss')
         .pipe( sass({
+            outputStyle: 'compressed',
             includePaths: require('node-normalize-scss').includePaths
         }) )
         .pipe( sass())
@@ -24,9 +26,18 @@ gulp.task('fonts', function() {
         .pipe( gulp.dest('build/fonts/') );
 });
 
+gulp.task('img', function () {
+    gulp.src('./src/images/*')
+        .pipe( imagemin({
+            progressive: true,
+            optimizationLevel: 8
+        }) )
+        .pipe( gulp.dest('build/images') );
+});
+
 gulp.task('watch', function() {
     gulp.watch('src/sass/**/*.scss', ['sass']);
     gulp.watch('src/templates/**/*.pug', ['pug']);
 });
 
-gulp.task('build', ['sass', 'pug', 'fonts']);
+gulp.task('build', ['sass', 'pug', 'fonts', 'img']);
