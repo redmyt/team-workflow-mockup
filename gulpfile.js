1e72369648f07615df603adf33b853370646cfcf
iriza7 <irynabautista@gmail.com>
2017-06-06 00:17:51 +0300

    'refactoring_gulpfile:_use_4_space_indentation'

--- gulpfile.js
+++ gulpfile.js
@@ -5,39 +5,39 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     cleanCSS = require('gulp-clean-css');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     gulp.src('./src/sass/main.scss')
-        .pipe( sass({
+        .pipe(sass({
             outputStyle: 'compressed',
             includePaths: require('node-normalize-scss').includePaths
-        }) )
-        .pipe( sass() )
-        .pipe( autopref() )
+        }))
+        .pipe(sass())
+        .pipe(autopref())
         // .pipe( cleanCSS() )
-        .pipe( gulp.dest('build/css') );
+        .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('pug', function() {
+gulp.task('pug', function () {
     gulp.src('./src/templates/index.pug')
-        .pipe( pug() )
-        .pipe( gulp.dest('build/') );
+        .pipe(pug())
+        .pipe(gulp.dest('build/'));
 });
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function () {
     gulp.src('./src/fonts/*')
-        .pipe( gulp.dest('build/fonts/') );
+        .pipe(gulp.dest('build/fonts/'));
 });
 
 gulp.task('img', function () {
     gulp.src('./src/images/*')
-        .pipe( imagemin({
+        .pipe(imagemin({
             progressive: true,
             optimizationLevel: 8
-        }) )
-        .pipe( gulp.dest('build/images') );
+        }))
+        .pipe(gulp.dest('build/images'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('src/sass/**/*.scss', ['sass']);
     gulp.watch('src/templates/**/*.pug', ['pug']);
 });
